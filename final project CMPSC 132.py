5de2f0644596f711256d1729b62a02433f348154
santiagoprzz <santiagoaperez2007@gmail.com>
2026-04-28 20:07:10 -0400

    Changed some of the code and added a function for new hints

--- final project CMPSC 132.py
+++ final project CMPSC 132.py
@@ -1,17 +1,25 @@
 import random
 
-choose_difficulty= input("Choose difficulty (easy / medium / hard): ")
+def proximity_hint(guess,number):   #Function that takes two arguments and selects which hints to give out
+    distance= abs(guess -number)
+    if distance<= 4:
+        print("Super close!")
+    elif distance<= 10:
+        print("Close")
+    else:
+        print("Far away")
+
+
+choose_difficulty= input("Choose difficulty (easy / medium / hard): ")      #Player chooses difficulty
 choose_difficulty= choose_difficulty.lower()
 max_attempts= None
 if choose_difficulty == "hard":
     max_attempts= 7
 
-
 number= random.randint(1, 100)
 attempts= 0
 GUESSED= False
 valid_input = False
-
 print("THE INCREDIBLE NUMBER GUESSING GAME :D")
 print("Are you ready??????")
 print("LETS START")
@@ -21,22 +29,31 @@ while GUESSED is not True and (max_attempts is None or attempts < max_attempts):
     valid_input = False
     guess= input("Please insert your guess: ")
 
-    if guess.isdigit():            
+    if guess.isdigit():            #Check if input is a number, else ask again
         guess = int(guess)
         valid_input = True
     else:
         print("Please choose a number")
 
-    if valid_input is True:                     
+    if valid_input is True:                            #Tracks attempts
         attempts +=1
-        if guess> number:
-            print("Too high")
-        elif guess < number:
-            print("Too low")
+
+        #FOR EASY MODE  
+        if choose_difficulty=="easy":
+            if guess> number:
+                print("Too high")
+            elif guess< number:
+                print("Too low")
+
+        #FOR MEDIUM/HARD MODE
         else:
-            GUESSED=True
+            proximity_hint(guess, number)
+        
+        if guess==number:     
+            GUESSED = True
             print("CONGRATS you guessed correctly!!!!!!")
             print(f"It took you {attempts} attempts.")
+        
     
-    if attempts> max_attempts:
-        print(f"GAME OVER!! Correct answer was: {number}")
+if GUESSED is not True:
+    print(f"GAME OVER!! Correct answer was: {number}")
